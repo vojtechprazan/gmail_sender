53af396d8d7be4e62408952088301f7620d01699
me <xx@seznam.cz>
2024-07-07 23:44:57 +0200

    more stable version

--- main.py
+++ main.py
@@ -23,6 +23,27 @@ EMAIL_BODY = 'Test Email Body'
 PASSWORD = os.getenv('PASSWORD_ENV_VAR')
 
 
+# Decorator to retry interaction with driver before calling the function
+def renew_driver(func):
+    @wraps(func)
+    def wrapper(driver, *args, **kwargs):
+        max_retries = 3
+        for attempt in range(max_retries):
+            try:
+                # Call the function with the driver instance
+                return func(driver, *args, **kwargs)
+            except Exception as e:
+                print(f"Attempt {attempt + 1} failed: {e}")
+                if attempt < max_retries - 1:
+                    print("Retrying...")
+                    continue  # Retry the function
+                else:
+                    print(f"Max retries ({max_retries}) exceeded. Giving up.")
+                    raise  # Re-raise the exception after max retries
+
+    return wrapper
+
+
 class Button:
     def __init__(self, driver, xpath):
         self.driver = driver
@@ -61,6 +82,7 @@ def login(driver):
 
         # Wait for the password input field to be present
         password_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "Passwd")))
+
         password_input.send_keys(PASSWORD)
         password_input.send_keys(Keys.ENTER)
 
@@ -75,52 +97,58 @@ def login(driver):
         driver.quit()
 
 
-def compose_and_send_email(driver):
+def compose_email(driver):
+    # Click on the Compose button
+    compose_button = Button(driver, "//div[@role='button' and text()='Compose']")
+    compose_button.find_and_click_button()
+
+    # Wait for the recipient input field to be present
+    to_input = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//input[@aria-label='To recipients']")))
 
     try:
-        # Click on the Compose button
-        compose_button = Button(driver, "//div[@role='button' and text()='Compose']")
-        compose_button.find_and_click_button()
-
-        # Wait for the recipient input field to be present
-        to_input = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, "//input[@aria-label='To recipients']"))
-        )
-        sleep(1)
+        to_input.send_keys(EMAIL)
+    except:
+        # i think this is cheating
+        element = driver.find_element(By.XPATH, "//div[contains(@class, 'fX') and contains(@class, 'aiL')]")
+        driver.execute_script("arguments[0].style = '';", element)
         to_input.send_keys(EMAIL)
 
-        # Enter the subject
-        subject_input = driver.find_element(By.NAME, "subjectbox")
-        subject_input.send_keys(EMAIL_SUBJECT)
+    # Enter the subject
+    subject_input = driver.find_element(By.NAME, "subjectbox")
+    subject_input.send_keys(EMAIL_SUBJECT)
 
-        # Enter the email body
-        body_input = driver.find_element(By.XPATH, "//div[@aria-label='Message Body']")
-        body_input.send_keys(EMAIL_BODY)
-        print("Step 2: Pass!  Mail composed")
-
-        # click more options
-        more_options = driver.find_element(By.XPATH, "//div[@data-tooltip='More options']")
-        more_options.click()
-
-        # click label
-        label_as_option = driver.find_element(By.XPATH, "//div[contains(text(), 'Label')]")
-        label_as_option.click()
-
-        # click social
-        social_label_option = driver.find_element(By.XPATH, "//div[contains(text(), 'Social')]")
-        social_label_option.click()
-        print("Step 3: Pass!  Social label selected")
-
-        # Click on the Send button
-        send_button = Button(driver, "//div[@role='button' and text()='Send']")
-        send_button.find_and_click_button()
-        print("Step 4: Pass! Mail sent")
-
-    except Exception as e:
-        driver.quit()
-        print(f"An error {e} occurred while executing Step 2, 3, 4")
+    # Enter the email body
+    body_input = driver.find_element(By.XPATH, "//div[@aria-label='Message Body']")
+    body_input.send_keys(EMAIL_BODY)
+    print("Step 2: Pass!  Mail composed")
 
 
+@renew_driver
+def mark_email_as_label(driver):
+    # click more options
+    more_options = driver.find_element(By.XPATH, "//div[@data-tooltip='More options']")
+    more_options.click()
+
+    # click label
+    label_as_option = driver.find_element(By.XPATH, "//div[contains(text(), 'Label')]")
+    label_as_option.click()
+
+    # click social
+    social_label_option = driver.find_element(By.XPATH, "//div[contains(text(), 'Social')]")
+    social_label_option.click()
+    print("Step 3: Pass!  Social label selected")
+
+
+@renew_driver
+def send_email(driver):
+    # Click on the Send button
+    send_button = Button(driver, "//div[@role='button' and text()='Send']")
+    send_button.find_and_click_button()
+    print("Step 4: Pass! Mail sent")
+
+
+@renew_driver
 def get_number_of_inbox(driver):
     inbox = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.XPATH, "//div[@data-tooltip='Inbox']"))
@@ -142,6 +170,7 @@ def verify_two_emails(old, new):
         raise ValueError("Mail from different sender arrived during test")
 
 
+@renew_driver
 def verify_new_mail_came(driver, old_count, old_email):
     timeout = 30  # seconds
     interval = 1  # seconds
@@ -159,18 +188,98 @@ def verify_new_mail_came(driver, old_count, old_email):
 
 
 def get_inboxes(driver):
-    emails = WebDriverWait(driver, 30).until(
+    emails = WebDriverWait(driver, 10).until(
         EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div.Cp div table.F.cf.zt tbody tr"))
     )
     return emails
 
 
-def get_newest_inbox(driver):
+@renew_driver
+def mark_first_email_as_starred(driver):
     emails = get_inboxes(driver)
+    first_email = emails[0]
+    star_icon = first_email.find_element(By.CSS_SELECTOR, "span.aXw.T-KT[aria-label='Not starred']")
+    star_icon.click()
+    print("Step 6: Pass! First email marked as starred.")
+
+
+@renew_driver
+def open_received_email(driver):
+    emails = get_inboxes(driver)
+
+    first_email = emails[0]
+
+    # Locate the subject within the first email row
+    subject_element = first_email.find_element(By.CSS_SELECTOR, "span.bqe")
+
+    # Ensure the subject element is visible
+    if subject_element.is_displayed():
+        # Scroll the subject element into view and click on it
+        actions = ActionChains(driver)
+        actions.move_to_element(subject_element).perform()
+        subject_element.click()
+    else:
+        # Use JavaScript to click the element if it is not directly clickable
+        driver.execute_script("arguments[0].click();", subject_element)
+    print("Step 7: Pass! First email opened.")
+
+
+@renew_driver
+def check_if_mail_is_social(driver):
+    # click more options
+    parent_div = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, '//div[@class="iH bzn"]'))
+    )
+
+    # Now find the "more options" button within the parent div
+    more_options_button = parent_div.find_element(By.XPATH, './/div[@aria-label="More email options"]')
+
+    more_options_button.click()
+
+    # click label
+    label_as_element = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, 'div.J-N.J-Ph'))
+    )
+    label_as_element.click()
+
+    social_element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, 'div.J-LC[title="Social"]'))
+    )
+
+    aria_checked = social_element.get_attribute("aria-checked")
+    if aria_checked == "true":
+        print("Step 8 Pass! Social label verified")
+    else:
+        print(f"Step 8 Fail! Social label value {aria_checked}")
+
+
+@renew_driver
+def verify_subject_and_body(driver):
+    email_body_parent = driver.find_element(By.XPATH, "//*[contains(@class, 'a3s') and contains(@class, 'aiL')]")
+    email_body_text = email_body_parent.find_element(By.XPATH, ".//div[@dir='ltr']").text
+
+    if email_body_text == EMAIL_BODY:
+        print("Step 9  1/2 Pass! Email body verified")
+    else:
+        print(f"Step 9 1/2 Fail! Email body value {email_body_text}")
+
+    email_list = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, 'div.Cp div table.F.cf.zt tbody')))
+    subject_text = email_list.find_element(By.XPATH, '//h2[contains(@class, "hP")]').text
+
+    if subject_text == EMAIL_SUBJECT:
+        print("Step 9  2/2 Pass! Email subject verified")
+    else:
+        print(f"Step 9 2/2 Fail! Email subject value {subject_text}")
+
+
+@renew_driver
+def get_newest_inbox(driver):
+    emails = driver.find_elements(By.CSS_SELECTOR, "div.Cp div table.F.cf.zt tbody tr")
+
     if emails:
-        sender_object = emails[0].find_element(By.CSS_SELECTOR, "span.zF")
-        sender = sender_object.get_attribute("name")
-        subject = emails[0].find_element(By.CSS_SELECTOR, "span.bog").text
+        sender = emails[0].find_element(By.XPATH, ".//td[4]").text
+        subject = emails[0].find_element(By.XPATH, ".//td[5]/div/div/div").text
         time = emails[0].find_element(By.CSS_SELECTOR, "td.xW.xY span").get_attribute('title')
         return sender, subject, time
     else:
@@ -195,94 +304,29 @@ def main():
     else:
         old_email = Email(*get_newest_inbox(firefox))
 
-    # step 2, 3, 4 - compose email, label social, send email
-    compose_and_send_email(firefox)
+    # step 2 compose email
+    compose_email(firefox)
+
+    # step 3 mark email as social
+    mark_email_as_label(firefox)
+
+    # step 4 send email
+    send_email(firefox)
 
     # step 5 verify new mail has come
     verify_new_mail_came(firefox, old_inbox_count, old_email)
 
     # step 6 mark first email as starred
-    emails = get_inboxes(firefox)
-
-    try:
-        first_email = emails[0]
-        star_icon = first_email.find_element(By.CSS_SELECTOR, "span.aXw.T-KT[aria-label='Not starred']")
-        star_icon.click()
-        print("Step 6: Pass! First email marked as starred.")
-
-    except Exception as e:
-        print(f"Step 6: Fail! An error occurred while starring the email: {e}")
+    mark_first_email_as_starred(firefox)
 
     # step 7 open received email
-    try:
-        first_email = emails[0]
-
-        # Locate the subject within the first email row
-        subject_element = first_email.find_element(By.CSS_SELECTOR, "span.bqe")
-
-        # Ensure the subject element is visible
-        if subject_element.is_displayed():
-            # Scroll the subject element into view and click on it
-            actions = ActionChains(firefox)
-            actions.move_to_element(subject_element).perform()
-            subject_element.click()
-        else:
-            # Use JavaScript to click the element if it is not directly clickable
-            firefox.execute_script("arguments[0].click();", subject_element)
-        print("Step 7: Pass! First email opened.")
-    except Exception as e:
-        print(f"Step 7: Fail! An error occurred while opening the email: {e}")
+    open_received_email(firefox)
 
     # step 8 check if the mail is in social
-    # click more options
-    more_options_button = WebDriverWait(firefox, 10).until(
-        EC.element_to_be_clickable((By.CSS_SELECTOR, 'div[id=":ao"]'))
-    )
-    more_options_button.click()
-
-    # click label
-    label_as_element = WebDriverWait(firefox, 10).until(
-        EC.element_to_be_clickable((By.CSS_SELECTOR, 'div.J-N.J-Ph'))
-    )
-    label_as_element.click()
-
-    social_element = WebDriverWait(firefox, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, 'div.J-LC[title="Social"]'))
-    )
-
-    aria_checked = social_element.get_attribute("aria-checked")
-    if aria_checked == "true":
-        print("Step 8 Pass! Social label verified")
-    else:
-        print(f"Step 8 Fail! Social label value {aria_checked}")
+    check_if_mail_is_social(firefox)
 
     # step 9 verify subject and body of received email
-    email_list = WebDriverWait(firefox, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, 'div.Cp div table.F.cf.zt tbody')))
-    subject_element = email_list.find_element(By.XPATH, '//h2[contains(@class, "hP")]')
-
-    email_body_element = WebDriverWait(firefox, 10).until(
-        EC.presence_of_element_located((By.XPATH, '//div[@id=":43"]//div[@dir="ltr"]'))
-    )
-
-    email_body_element = WebDriverWait(firefox, 10).until(
-        EC.presence_of_element_located((By.XPATH, '//div[@id=":43"]//div[@dir="ltr"]'))
-    )
-
-    if email_body_element.text == EMAIL_BODY:
-        print("Step 9  1/2 Pass! Email body verified")
-    else:
-        print(f"Step 9 1/2 Fail! Email body value {email_body_element.text}")
-
-    if subject_element.text == EMAIL_SUBJECT:
-        print("Step 9  2/2 Pass! Email subject verified")
-    else:
-        print(f"Step 9 2/2 Fail! Email subject value {subject_element.text}")
-
-    if subject_element.text == EMAIL_SUBJECT:
-        print("Step 9  1/2 Pass! Email subject verified")
-    else:
-        print(f"Step 9 1/2 Fail! Email subject value {subject_element.text}")
+    verify_subject_and_body(firefox)
 
     # quit
     quit_browser(firefox)
